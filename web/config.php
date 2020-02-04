31b91f13b89044d29f1f7a4371f00b92d135502b
lixiaolong1997 <lixiaolong1997@126.com>
2020-02-04 15:51:49 +0800

    Update config.php

--- web/config.php
+++ web/config.php
@@ -1,10 +1,10 @@
 <?php
 $gl=(isset($_COOKIE['gl']) && $_COOKIE['gl'])?$_COOKIE['gl']:'US';
 define('ROOT_PART', Root_part());
-define('APIKEY', 'AIzaSyBmYQPhWRuVcFG8XNO-WDBFLZpZtIkKlbY');
+define('APIKEY', 'AIzaSyDRXOAT7kaxgsuweQKGBrsNTBwrh_uWTsk');
 define('GJ_CODE', $gl);
-define('SITE_NAME', 'Hello World!');
-define('TITLENAME', 'Hello World!');
+define('SITE_NAME', 'Hello Y2B!');
+define('TITLENAME', 'Hello Y2B!');
 define('EN2DEKEY', 'AnQUP1XDEF87ewemhuYG35e67CtzDSh4');
 define('EMAIL', 'None@gmail.com');
 ?>
